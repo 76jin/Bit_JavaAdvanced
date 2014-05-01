a23bb15d8c8456c5941f4ab182ace5f30cfe3203
ranian129 <ranian129@gmail.com>
2014-05-01 17:36:48 +0900

    update mybatis

--- web02/src/main/java/sems/controls/auth/LoginControl.java
+++ web02/src/main/java/sems/controls/auth/LoginControl.java
@@ -1,5 +1,7 @@
 package sems.controls.auth;
 
+import java.util.HashMap;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -41,25 +43,30 @@ public class LoginControl {
 			HttpSession session,
 			HttpServletResponse response) {	 // 로그인 수행, 파라미터는 프런트 컨트롤러가 알아서 꼽아줌
 		try {
-				UserVo userVo = null;
-				try {
-					userVo = userDao.getUser(email, password);
-					
-				} catch (DaoException e) {		// 로그인 실패!
-					return "redirect:login.bit";
-				}
+			
+			UserVo userVo = null;
+			try {
+				HashMap<String,String> params = new HashMap<String,String>();
+				params.put("email", email);
+				params.put("password", password);
 				
-				// 로그인 성공인 경우 처리
-				session.setAttribute("loginUser", userVo);
-				
-				if (saveEmail != null) {
-					Cookie cookie = new Cookie("loginEmail", email);
-					cookie.setDomain("s24.java48.com");		// 서버 범위
-					cookie.setPath("/web02");							// 서버의 하위 폴더 범위
-					//cookie.setMaxAge(40);               // 유효기간 지정 : 40초
-					
-					response.addCookie(cookie);
-				}
+				userVo = userDao.getUser(params);
+
+			} catch (DaoException e) {		// 로그인 실패!
+				return "redirect:login.bit";
+			}
+
+			// 로그인 성공인 경우 처리
+			session.setAttribute("loginUser", userVo);
+
+			if (saveEmail != null) {
+				Cookie cookie = new Cookie("loginEmail", email);
+				cookie.setDomain("s24.java48.com");		// 서버 범위
+				cookie.setPath("/web02");							// 서버의 하위 폴더 범위
+				//cookie.setMaxAge(40);               // 유효기간 지정 : 40초
+
+				response.addCookie(cookie);
+			}
 				
 				return "redirect:../subject/list.bit?pageNo=1&pageSize=10";
 		} catch (Throwable ex) {
